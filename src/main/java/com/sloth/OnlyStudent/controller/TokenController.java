14275a94fe992b54e83810b08acf02160495cd9c
YuriNaoki <dev.peterson.machado@gmail.com>
2024-08-25 21:58:15 -0300

    Validando Token s2

--- src/main/java/com/sloth/OnlyStudent/controller/TokenController.java
+++ src/main/java/com/sloth/OnlyStudent/controller/TokenController.java
@@ -1,5 +1,7 @@
 package com.sloth.OnlyStudent.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +15,8 @@ import com.sloth.OnlyStudent.infra.security.TokenService;
 @RestController
 @RequestMapping("token")
 public class TokenController {
-
+	
+	private static final Logger logger = LoggerFactory.getLogger(AuthenticationController.class);
 	@Autowired
 	private TokenService tokenService;
 	
@@ -24,11 +27,16 @@ public class TokenController {
         try {
             String email = tokenService.validateToken(token);
             if (email != null) {
+            	logger.info("Token está válido!");
                 return ResponseEntity.ok("Token is valid.");
             } else {
+
+            	logger.info("Token está inválido!");
                 return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Token is invalid or expired.");
             }
         } catch (Exception e) {
+
+        	logger.info("Token está inválido!");
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Token is invalid or expired.");
         }
     }
